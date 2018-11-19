c3fd893dc72eaa44b034d60945bdc32f5b9b657b
Timmy Christensen <timch15@student.sdu.dk>
2018-11-19 14:51:40 +0100

    Building init

---
+++ src/Domain/Building.java
@@ -0,0 +1,67 @@
+package Domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author timch15
+ */
+public class Building extends Unit {
+
+    private final String address;
+    private List<Actuator> actuatorList;
+    private List<Sensor> sensorList;
+
+    public Building(String name, String address) {
+        super(name);
+        this.address = address;
+        this.actuatorList = new ArrayList<>();
+        this.sensorList = new ArrayList<>();
+    }
+
+    public void addSensor(Sensor sensor) {
+        sensorList.add(sensor);
+    }
+
+    public void removeSensor(UUID id) {
+        for (int i = 0; i < sensorList.size(); i++) {
+            if (id == sensorList.get(i).getUUID()) {
+                sensorList.remove(i);
+                return;
+            }
+        }
+    }
+
+    public void addAcuator(Actuator actuator) {
+        actuatorList.add(actuator);
+    }
+
+    public void removeActuator(UUID id) {
+        for (int i = 0; i < actuatorList.size(); i++) {
+            if (id == actuatorList.get(i).getUUID()) {
+                actuatorList.remove(i);
+                return;
+            }
+        }
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public List<Actuator> getActuatorList() {
+        return actuatorList;
+    }
+
+    public List<Sensor> getSensorList() {
+        return sensorList;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + super.getName() + " Address: " + this.address;
+    }
+
+}
