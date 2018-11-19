b30de3f75a9f8b8687887843305f8d4743176cf5
AskeJL <aske.lorenz@gmail.com>
2018-11-19 14:46:40 +0100

    Sensor

---
+++ src/Domain/TempSensor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+/**
+ *
+ * @author askel
+ */
+public class TempSensor extends Sensor {
+
+    public TempSensor(String name) {
+        super(name);
+    }
+
+    @Override
+    public int getValue() {
+        return (int)Math.random()*100;
+    }
+
+    @Override
+    public String toString() {
+        
+        return super.getName() + super.getUUID() + this.getValue();
+    }
+    
+}
